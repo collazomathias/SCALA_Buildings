5062f06ea3415cd14191e38b6e3ace8fafabd186
Mathías Collazo <mathias.collazo.episcopo@gmail.com>
2022-06-06 10:54:59 -0300

    Added function to show all the data and some fixes.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,18 +4,23 @@ import build._
 import citadel.Citadel
 import buildorder.BuildOrder
 import java.time.LocalDate
+import request.Request
 
 object Main extends App {
+    val finishedBuilds : List[(Build, (Integer, Integer))] = List()
+    val ordersInProgress : List[BuildOrder] = List()
+    val requests : List[Request] = List()
     val inventory = Inventory(500, 500, 500, 500, 500)
     val day = LocalDate.now().getDayOfMonth()
     val month = LocalDate.now().getMonthValue()
     val year = LocalDate.now().getYear()
-    val citadel = Citadel(List(), List(), List(), inventory, day, month, year)
+    val citadel = Citadel(finishedBuilds, ordersInProgress, requests, inventory, day, month, year)
     val citadel2 = Functions.createRequest(citadel, Edificio, 5, 5)
     val citadel3 = Functions.createRequest(citadel2, Casa, 6, 6)
-    val citadel4 = Functions.createRequest(citadel3, Edificio, 7, 7)
+    val citadel4 = Functions.createRequest(citadel3, Edificio, 5, 5)
     val citadel5 = Functions.createRequest(citadel4, Edificio, 8, 8)
-    //val citadel4 = Functions.updateOrders(citadel3)
+    val citadel6 = Functions.updateOrders(citadel5)
+    val citadel7 = Functions.updateOrders(citadel6)
     //val citadel5 = Functions.updateOrders(citadel4)
-    println(citadel5)
+    Utils.showInformation(citadel7)
 }
--- src/main/scala/functions/Functions.scala
+++ src/main/scala/functions/Functions.scala
@@ -7,6 +7,7 @@ import request.Request
 import buildorder.BuildOrder
 import java.util.Calendar
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 trait Errors {
     type Coordinate = Integer
@@ -40,6 +41,7 @@ object Errors extends Errors {
 trait Utils {
   def getBuildType(build : Build) : String 
   def getFinishDate(citadel : Citadel) : String
+  def showInformation(citadel : Citadel) : Unit
 }
 
 object Utils extends Utils {
@@ -59,6 +61,67 @@ object Utils extends Utils {
         date
     }
 
+    def showInformation(citadel: Citadel): Unit = {
+        println(s"Inventory: \n\tArena: ${citadel.inventory.arena} " +
+            s"\n\tGrava: ${citadel.inventory.grava} " +
+            s"\n\tCemento: ${citadel.inventory.cemento} " +
+            s"\n\tMadera: ${citadel.inventory.madera} " +
+            s"\n\tAdobe: ${citadel.inventory.adobe}")
+        if(citadel.finishedBuilds.isEmpty) println("Finished builds: Finished builds not found.")
+        else {
+            println("Finished builds:")
+            citadel.finishedBuilds.foreach(build => {
+                build._1 match {
+                    case Casa => println(s"\tBuild type: Casa" +
+                        s"Coordinates: (${build._2._1}, ${build._2._2})")
+                    case Edificio => println(s"\tBuild type: Edificio" +
+                        s"Coordinates: (${build._2._1}, ${build._2._2})")
+                    case Lago => println(s"\tBuild type: Lago" +
+                        s"Coordinates: (${build._2._1}, ${build._2._2})")
+                    case CanchaDeFutbol => println(s"\tBuild type: Cancha de fútbol" +
+                        s"Coordinates: (${build._2._1}, ${build._2._2})")
+                    case Gimnasio => println(s"\tBuild type: Gimnasio" +
+                        s"Coordinates: (${build._2._1}, ${build._2._2})")
+                    case _ => println("\tBuild type not found")
+                }
+            })
+        }
+        if(citadel.ordersInProgress.isEmpty) println("Build orders: Build orders not found.")
+        else {
+            println(s"Build orders:")
+            citadel.ordersInProgress.foreach(order => {
+                order.build match {
+                    case Casa => println(s"\tBuild type: Casa" +
+                        s"\tCoordinates: (${order.x_coord},${order.y_coord})" +
+                        s"\tStart day: ${order.startDay}/${order.startMonth}/${order.startYear}" +
+                        s"\tEnd day: ${order.endDay}/${order.endMonth}/${order.endYear}" +
+                        s"\tStatus: ${order.status}")
+                    case Edificio => println(s"\tBuild type: Edificio" +
+                        s"\tCoordinates: (${order.x_coord},${order.y_coord})" +
+                        s"\tStart day: ${order.startDay}/${order.startMonth}/${order.startYear}" +
+                        s"\tEnd day: ${order.endDay}/${order.endMonth}/${order.endYear}" +
+                        s"\tStatus: ${order.status}")
+                    case Lago => println(s"\tBuild type: Lago" +
+                        s"\tCoordinates: (${order.x_coord},${order.y_coord})" +
+                        s"\tStart day: ${order.startDay}/${order.startMonth}/${order.startYear}" +
+                        s"\tEnd day: ${order.endDay}/${order.endMonth}/${order.endYear}" +
+                        s"\tStatus: ${order.status}")
+                    case CanchaDeFutbol => println(s"\tBuild type: Cancha de fútbol" +
+                        s"\tCoordinates: (${order.x_coord},${order.y_coord})" +
+                        s"\tStart day: ${order.startDay}/${order.startMonth}/${order.startYear}" +
+                        s"\tEnd day: ${order.endDay}/${order.endMonth}/${order.endYear}" +
+                        s"\tStatus: ${order.status}")
+                    case Gimnasio => println(s"\tBuild type: Gimnasio" +
+                        s"\tCoordinates: (${order.x_coord},${order.y_coord})" +
+                        s"\tStart day: ${order.startDay}/${order.startMonth}/${order.startYear}" +
+                        s"\tEnd day: ${order.endDay}/${order.endMonth}/${order.endYear}" +
+                        s"\tStatus: ${order.status}")
+                    case _ => println(s"\tBuild type not found")
+                }
+            })
+        }
+        println(s"Building end date: ${citadel.day}/${citadel.month}/${citadel.year}.")
+    }
 }
 
 trait Functions {
@@ -69,57 +132,89 @@ trait Functions {
 }
 
 object Functions extends Functions {
-    def createRequest(citadel : Citadel, build: Build, x: Coordinate, y: Coordinate): Citadel = {
+    def createRequest(citadel : Citadel, build: Build, x: Coordinate, y: Coordinate) : Citadel = {
         if(Errors.getMissingMaterial(citadel.inventory, build)) {
             println("There is not enough material to build.")
             return citadel
         } else if(Errors.getExistingCoords(citadel, x, y)) {
-            println("A build already exists in that position.")
+            println("A build already exists in that position, or is in a 'Pending/In-progress' state.")
             return citadel
         }
         val request = Request(build, x, y)
-        val buildOrderDate = LocalDate.of(citadel.year, citadel.month, citadel.day).plusDays(1)
-        val buildOrderDay = buildOrderDate.getDayOfMonth()
-        val buildOrderMonth = buildOrderDate.getMonthValue()
-        val buildOrderYear = buildOrderDate.getYear()
-        val buildOrderFinishDate = LocalDate.of(buildOrderYear, buildOrderMonth, buildOrderDay).plusDays(build.buildingTime.toLong)
-        val buildOrderFinishDay = buildOrderFinishDate.getDayOfMonth()
-        val buildOrderFinishMonth = buildOrderFinishDate.getMonthValue()
-        val buildOrderFinishYear = buildOrderFinishDate.getYear()
-        val buildOrder = BuildOrder(build, x, y, buildOrderDay, buildOrderMonth, buildOrderYear, buildOrderFinishDay, buildOrderFinishMonth, buildOrderFinishYear, "Pending")
-        println("You have processed the request successfully.")
-        citadel.copy(ordersInProgress = citadel.ordersInProgress.appended(buildOrder), 
-                    requests = citadel.requests.appended(request),
-                    day = buildOrderFinishDay,
-                    month = buildOrderFinishMonth,
-                    year = buildOrderFinishYear,
-                    inventory = citadel.inventory.copy(cemento = citadel.inventory.cemento - build.necessaryCemento,
-                                                        grava = citadel.inventory.grava - build.necessaryGrava,
-                                                        arena = citadel.inventory.arena - build.necessaryArena,
-                                                        madera = citadel.inventory.madera - build.necessaryMadera,
-                                                        adobe = citadel.inventory.adobe - build.necessaryAdobe))
+        if(LocalDate.of(citadel.year, citadel.month, citadel.day).plusDays(1).isBefore(LocalDate.now())) {
+            val buildOrderDate = LocalDate.now().plusDays(1)
+            val buildOrderDay = buildOrderDate.getDayOfMonth()
+            val buildOrderMonth = buildOrderDate.getMonthValue()
+            val buildOrderYear = buildOrderDate.getYear()
+            val buildOrderFinishDate = LocalDate.of(buildOrderYear, buildOrderMonth, buildOrderDay).plusDays(build.buildingTime.toLong)
+            val buildOrderFinishDay = buildOrderFinishDate.getDayOfMonth()
+            val buildOrderFinishMonth = buildOrderFinishDate.getMonthValue()
+            val buildOrderFinishYear = buildOrderFinishDate.getYear()
+            val buildOrder = BuildOrder(build, x, y, buildOrderDay, buildOrderMonth, buildOrderYear, buildOrderFinishDay, buildOrderFinishMonth, buildOrderFinishYear, "Pending")
+            println("You have processed the request successfully.")
+            citadel.copy(ordersInProgress = citadel.ordersInProgress.appended(buildOrder), 
+                        requests = citadel.requests.appended(request),
+                        day = buildOrderFinishDay,
+                        month = buildOrderFinishMonth,
+                        year = buildOrderFinishYear,
+                        inventory = citadel.inventory.copy(cemento = citadel.inventory.cemento - build.necessaryCemento,
+                            grava = citadel.inventory.grava - build.necessaryGrava,
+                            arena = citadel.inventory.arena - build.necessaryArena,
+                            madera = citadel.inventory.madera - build.necessaryMadera,
+                            adobe = citadel.inventory.adobe - build.necessaryAdobe))
+        } else {
+            val buildOrderDate = LocalDate.of(citadel.year, citadel.month, citadel.day).plusDays(1)
+            val buildOrderDay = buildOrderDate.getDayOfMonth()
+            val buildOrderMonth = buildOrderDate.getMonthValue()
+            val buildOrderYear = buildOrderDate.getYear()
+            val buildOrderFinishDate = LocalDate.of(buildOrderYear, buildOrderMonth, buildOrderDay).plusDays(build.buildingTime.toLong)
+            val buildOrderFinishDay = buildOrderFinishDate.getDayOfMonth()
+            val buildOrderFinishMonth = buildOrderFinishDate.getMonthValue()
+            val buildOrderFinishYear = buildOrderFinishDate.getYear()
+            val buildOrder = BuildOrder(build, x, y, buildOrderDay, buildOrderMonth, buildOrderYear, buildOrderFinishDay, buildOrderFinishMonth, buildOrderFinishYear, "Pending")
+            println("You have processed the request successfully.")
+            citadel.copy(ordersInProgress = citadel.ordersInProgress.appended(buildOrder), 
+                        requests = citadel.requests.appended(request),
+                        day = buildOrderFinishDay,
+                        month = buildOrderFinishMonth,
+                        year = buildOrderFinishYear,
+                        inventory = citadel.inventory.copy(cemento = citadel.inventory.cemento - build.necessaryCemento,
+                            grava = citadel.inventory.grava - build.necessaryGrava,
+                            arena = citadel.inventory.arena - build.necessaryArena,
+                            madera = citadel.inventory.madera - build.necessaryMadera,
+                            adobe = citadel.inventory.adobe - build.necessaryAdobe))
+        }
+        
     }
 
     def addInventory(citadel : Citadel, quant : Integer): Citadel = {
         citadel.copy(inventory = citadel.inventory.copy(cemento = citadel.inventory.cemento + quant,
-                                                        grava = citadel.inventory.grava + quant,
-                                                        arena = citadel.inventory.arena + quant,
-                                                        madera = citadel.inventory.madera + quant,
-                                                        adobe = citadel.inventory.adobe + quant))
+            grava = citadel.inventory.grava + quant,
+            arena = citadel.inventory.arena + quant,
+            madera = citadel.inventory.madera + quant,
+            adobe = citadel.inventory.adobe + quant))
     }
 
     def updateOrders(citadel: Citadel): Citadel = {
         val trunced = citadel.ordersInProgress.take(1).head
+        val hour = LocalDateTime.now().getHour()
+        val day = LocalDate.now().getDayOfMonth()
+        val month = LocalDate.now().getMonthValue()
+        val year = LocalDate.now().getYear()
         if(trunced.status == "Pending" &&
-            LocalDate.now().getDayOfMonth() == trunced.startDay &&
-            LocalDate.now().getMonthValue() == trunced.startMonth &&
-            LocalDate.now().getYear() == trunced.startYear) {
+            day == trunced.startDay &&
+            month == trunced.startMonth &&
+            year == trunced.startYear &&
+            hour >= 6 &&
+            hour <= 12) {
             val newCitadel = citadel.copy(ordersInProgress = citadel.ordersInProgress.drop(1))
             return newCitadel.copy(ordersInProgress = List(trunced.copy(status = "In progress")) ++ newCitadel.ordersInProgress)
-        } else if(trunced.status == "In progress" &&
-            LocalDate.now().getDayOfMonth() == trunced.endDay &&
-            LocalDate.now().getMonthValue() == trunced.endMonth &&
-            LocalDate.now().getYear() == trunced.endYear) {
+        } else if(trunced.status == "In-progress" &&
+            day == trunced.endDay &&
+            month == trunced.endMonth &&
+            year == trunced.endYear &&
+            hour >= 17 &&
+            hour <= 23) {
             return citadel.copy(ordersInProgress = citadel.ordersInProgress.drop(1), finishedBuilds = citadel.finishedBuilds.appended((trunced.build, (trunced.x_coord, trunced.y_coord))))
         }
         citadel
